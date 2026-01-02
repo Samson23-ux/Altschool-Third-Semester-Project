d20db6230ca01e6cfa1b05bffe3dd04eb423e988
samsonajifowoke <samsonajifowoke@gmail.com>
2026-01-02 03:29:19 +0100

    updated dependencies

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,19 @@
 from pydantic_settings import BaseSettings
 
-class Settings(BaseSettings):
-    API_TITLE: str = 'Mini Social Media Feed API'
-    API_VERSION_1: str = '/api/v1'
-    DESCRIPTION: str = 'A REST API for a social media feed for altschool project'
 
+class Settings(BaseSettings):
+    API_TITLE: str = "Mini Social Media Feed API"
+    API_VERSION_1: str = "/api/v1"
+    DESCRIPTION: str = "A REST API for a social media feed for altschool project"
+
+    #DB
+    DATABASE_USERNAME: str
+    DATABASE_PASSWORD: str
     DATABASE_URL: str
 
-    class config:
-        env_file = '.env'
-        env_file_encoding = 'utf-8'
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
 
 settings = Settings()
